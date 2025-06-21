f2d45cf13c899e711f7f8e788481aaa3a0512d25
Ruslan Tugushev <rutug.official@gmail.com>
2025-06-21 20:43:26 +0400

    first commit

---
+++ src/test/java/github/SoftAssertionsTest.java
@@ -0,0 +1,27 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsTest {
+
+    @BeforeAll
+    static void setUp(){
+
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "";
+        Configuration.holdBrowserOpen = true;
+    }
+
+
+    @Test
+    void fillFormTest() {
+
+        //указываем нужную ссылку
+        open("");
+    }
+}
